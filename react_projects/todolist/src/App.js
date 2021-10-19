b56068e8e4e71af866f9ffd5b824e921e725b402
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-19 08:13:40 +0300

    update: complete section 5

--- react_projects/todolist/src/App.js
+++ react_projects/todolist/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState({ description: '', date: '' });
+	const [todos, setTodos] = useState([]);
+
+	const inputChanged = (event) => {
+		setTodo({ ...todo, [event.target.name]: event.target.value });
+	};
+
+	const addTodo = () => {
+		setTodos([...todos, todo]);
+		setTodo({ description: '', date: '' });
+	};
+
+	return (
+		<div className='App'>
+			<input
+				placeholder='Description'
+				name='description'
+				value={todo.description}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Date'
+				name='date'
+				value={todo.date}
+				onChange={inputChanged}
+			/>
+			<button onClick={addTodo}>Add</button>
+			<table>
+				<tbody>
+					{todos.map((todo, index) => (
+						<tr key={index}>
+							<td>{todo.description}</td>
+							<td>{todo.date}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;

5e6bfa3354bcdbf497fb0b4a66fce081f6eaf29a
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-19 07:21:59 +0300

    add: userinput to the file

--- react_projects/userinput/src/App.js
+++ react_projects/userinput/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('');
+
+	const inputChanged = (event) => {
+		setName(event.target.value);
+	};
+
+	return (
+		<div className='App'>
+			{name}
+			<br />
+			<input value={name} onChange={inputChanged} />
+		</div>
+	);
 }
 
 export default App;

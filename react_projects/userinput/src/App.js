bdb5d02e4f04a7b3531cf0b13a0d11e22e4be56c
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-19 07:39:52 +0300

    update: complete chapter 3

--- react_projects/userinput/src/App.js
+++ react_projects/userinput/src/App.js
@@ -2,17 +2,43 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [name, setName] = useState('');
+	const [person, setPerson] = useState({
+		firstname: '',
+		lastname: '',
+		email: '',
+	});
 
 	const inputChanged = (event) => {
-		setName(event.target.value);
+		setPerson({ ...person, [event.target.name]: event.target.value });
+	};
+
+	const showAlert = () => {
+		alert(`Hello ${person.firstname} ${person.lastname}`);
 	};
 
 	return (
 		<div className='App'>
-			{name}
+			Name: {person.firstname} {person.lastname} Email: {person.email}
 			<br />
-			<input value={name} onChange={inputChanged} />
+			<input
+				placeholder='First name'
+				name='firstname'
+				value={person.firstname}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Last name'
+				name='lastname'
+				value={person.lastname}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Email'
+				name='email'
+				value={person.email}
+				onChange={inputChanged}
+			/>
+			<button onClick={showAlert}>Submit</button>
 		</div>
 	);
 }

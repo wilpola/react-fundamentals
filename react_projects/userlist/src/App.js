0d35f38e38634a99522a23b5c37a377a4be8c705
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-19 19:57:11 +0300

    Complete quiz q: 2

--- react_projects/userlist/src/App.js
+++ react_projects/userlist/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const [trivia, setTrivia] = useState('');
+
+	useEffect(() => {
+		fetch('https://reqres.in/api/users')
+			.then((response) => response.json())
+			.then((resData) => setUsers(resData.data));
+		getTrivia();
+	}, []);
+
+	const getTrivia = () => {
+		fetch('https://opentdb.com/api.php?amount=1', { method: 'GET' })
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setTrivia(data.results[0].question);
+				console.log(data.results[0].question);
+			});
+	};
+
+	return (
+		<div className='App'>
+			<table>
+				<tbody>
+					{users.map((user, index) => (
+						<tr key={index}>
+							<td>{user.first_name}</td>
+							<td>{user.last_name}</td>
+							<td>{user.email}</td>
+							<td>
+								<img src={user.avatar} />
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<p>{trivia}</p>
+			<button onClick={getTrivia}>New question</button>
+		</div>
+	);
 }
 
 export default App;

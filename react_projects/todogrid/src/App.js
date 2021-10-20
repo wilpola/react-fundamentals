e7dacbd9ea6b9d497d07b144269484c658083405
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-20 06:39:06 +0300

    add: ag-grid to the app

--- react_projects/todogrid/src/App.js
+++ react_projects/todogrid/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState({ description: '', date: '', status: '' });
+	const [todos, setTodos] = useState([]);
+
+	const inputChanged = (event) => {
+		setTodo({ ...todo, [event.target.name]: event.target.value });
+	};
+
+	const addTodo = () => {
+		setTodos([...todos, todo]);
+		setTodo({ description: '', date: '', status: '' });
+	};
+
+	return (
+		<div className='App'>
+			<input
+				placeholder='Description'
+				name='description'
+				value={todo.description}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Date'
+				name='date'
+				value={todo.date}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Status'
+				name='status'
+				value={todo.status}
+				onChange={inputChanged}
+			/>
+			<button onClick={addTodo}>Add</button>
+			<div
+				className='ag-theme-material'
+				style={{ height: 600, width: 600, margin: 'auto' }}>
+				<AgGridReact rowData={todos}>
+					<AgGridColumn field='description'></AgGridColumn>
+					<AgGridColumn field='date'></AgGridColumn>
+					<AgGridColumn field='status'></AgGridColumn>
+				</AgGridReact>
+			</div>
+		</div>
+	);
 }
 
 export default App;

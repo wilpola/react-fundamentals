6b122b08fc08ae872f1f86fcfc7571ec53de2bf7
Wilpola - PC <71035737+wilpola@users.noreply.github.com>
2021-10-28 08:22:38 +0300

    add: material-ui

--- react_projects/materialtodo/src/App.js
+++ react_projects/materialtodo/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState({ description: '', date: '' });
+	const [todos, setTodos] = useState([]);
+
+	const inputChanged = (event) => {
+		setTodo({ ...todo, [event.target.name]: event.target.value });
+	};
+
+	const addTodo = () => {
+		setTodos([...todos, todo]);
+		setTodo({ description: '', date: '' });
+	};
+
+	const deleteTodo = (row) => {
+		setTodos(todos.filter((todo, index) => index !== row));
+	};
+
+	return (
+		<div className='App'>
+			<input
+				placeholder='Description'
+				name='description'
+				value={todo.description}
+				onChange={inputChanged}
+			/>
+			<input
+				placeholder='Date'
+				name='date'
+				value={todo.date}
+				onChange={inputChanged}
+			/>
+			<Button onClick={addTodo}>Add</Button>
+			<table>
+				<tbody>
+					{todos.map((todo, index) => (
+						<tr key={index}>
+							<td>{todo.description}</td>
+							<td>{todo.date}</td>
+							<td>
+								<IconButton
+									size='small'
+									color='secondary'
+									onClick={() => deleteTodo(index)}>
+									<DeleteIcon />
+								</IconButton>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
